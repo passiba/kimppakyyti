684a3272a50cc55fa1fde281ac82498b996ce986
passiba <passiba@gmail.com>
2014-10-29 11:40:49 +0200

    added introductory animation to front page menu modifications and added some images

---
+++ src/main/java/passiba/kimppakyyti2/users/views/TravellerImagesView.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passiba.kimppakyyti2.users.views;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+
+/**
+ *
+ * @author pauline
+ */
+@ManagedBean
+public class TravellerImagesView {
+    
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 5; i++) {
+            images.add("traveller" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+    
+}
